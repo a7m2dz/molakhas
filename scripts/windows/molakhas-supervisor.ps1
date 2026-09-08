2a362e51d6bdc60f7100d5e1362c8761e26a563d
a7m2dz <76899331+a7m2dz@users.noreply.github.com>
2026-09-08 12:06:42 +0300

    config: default Windows supervisor to auto/best-free

--- scripts/windows/molakhas-supervisor.ps1
+++ scripts/windows/molakhas-supervisor.ps1
@@ -38,7 +38,7 @@ Set-Location $ProjectRoot
 if (-not $env:PUBLIC_SITE_URL) { $env:PUBLIC_SITE_URL = 'https://mulakhas.com' }
 if (-not $env:OMNIROUTE_BASE_URL) { $env:OMNIROUTE_BASE_URL = 'http://127.0.0.1:20128/v1' }
 if (-not $env:OMNIROUTE_API_KEY) { $env:OMNIROUTE_API_KEY = 'sk_omniroute' }
-if (-not $env:OMNIROUTE_MODEL) { $env:OMNIROUTE_MODEL = 'auto/smart' }
+if (-not $env:OMNIROUTE_MODEL) { $env:OMNIROUTE_MODEL = 'auto/best-free' }
 if (-not $env:OMNIROUTE_FALLBACK_MODEL) { $env:OMNIROUTE_FALLBACK_MODEL = 'auto' }
 if (-not $env:OMNIROUTE_TIMEOUT_MS) { $env:OMNIROUTE_TIMEOUT_MS = '60000' }
 
